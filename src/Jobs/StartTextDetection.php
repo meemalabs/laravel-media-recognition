0eeb1b3cb8f8daa5135f10c9914c490106ba11a1
Chris <chrisbreuer93@gmail.com>
2021-01-03 13:51:59 -0800

    Apply fixes from StyleCI (#8)

--- src/Jobs/StartTextDetection.php
+++ src/Jobs/StartTextDetection.php
@@ -9,7 +9,6 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
-use Meema\MediaRecognition\Events\LabelAnalysisCompleted;
 use Meema\MediaRecognition\Events\TextAnalysisCompleted;
 use Meema\MediaRecognition\Facades\Recognize;
 
