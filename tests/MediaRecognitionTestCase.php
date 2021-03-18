f78434e751aaf413792d9e150421a563e6f11a0f
Glenn Michael Torregosa <gtorregosa@gmail.com>
2021-03-18 13:27:55 +0800

    test case typo

--- tests/MediaRecognitionTestCase.php
+++ tests/MediaRecognitionTestCase.php
@@ -9,8 +9,6 @@ use Orchestra\Testbench\TestCase;
 
 class MediaRecognitionTestCase extends TestCase
 {
-    public $jobSettings = [];
-
     protected function getPackageProviders($app)
     {
         return [MediaRecognitionServiceProvider::class];
@@ -29,8 +27,8 @@ class MediaRecognitionTestCase extends TestCase
     public function initializeSettings()
     {
         // let's make sure these config values are set
-        Config::set('media-converter.credentials.key', env('AWS_ACCESS_KEY_ID'));
-        Config::set('media-converter.credentials.secret', env('AWS_SECRET_ACCESS_KEY'));
+        Config::set('media-recognition.credentials.key', env('AWS_ACCESS_KEY_ID'));
+        Config::set('media-recognition.credentials.secret', env('AWS_SECRET_ACCESS_KEY'));
         Config::set('media-recognition.disk', env('REKOGNITION_DISK', 's3'));
         Config::set('media-recognition.iam_arn', env('AWS_IAM_REKOGNITION_ARN'));
         Config::set('media-recognition.sns_topic_arn', env('AWS_SNS_TOPIC_ARN'));
